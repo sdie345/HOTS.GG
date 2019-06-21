477f3c3ff198b118cfa22b8f89e8c1a4e1eacdd6
sdie345 <kst7703@gmail.com>
2019-06-21 13:04:05 +0900

    좆망겜

--- app/src/main/java/kr/swote/hotsgg/views/fragments/HeroFragment.java
+++ app/src/main/java/kr/swote/hotsgg/views/fragments/HeroFragment.java
@@ -2,10 +2,7 @@ package kr.swote.hotsgg.views.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,23 +22,5 @@ public class HeroFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_hero, container, false);
 
         return rootView;
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.search_menu, menu);
-        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
-            }
-        });
-        super.onCreateOptionsMenu(menu, inflater);
     }
 }
